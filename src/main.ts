805de71b38a4307f60cf8f9daa62b363ad375e05
Dieguidev <diegogaraycullas@gmail.com>
2024-04-26 08:40:54 -0500

    creado el createProduct dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      //forbidNonWhitelisted: true,
+      //*transform y transformOptions es para que los dto parseen los datos al tipo permitido
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  )
+
   await app.listen(3000);
 }
 bootstrap();
