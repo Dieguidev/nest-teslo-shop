55f1ac6cda93d6ef4fbb75216823572093f4fcf5
Dieguidev <diegogaraycullas@gmail.com>
2024-04-29 16:54:34 -0500

    utilizado el Auth decorator en otros modulos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,12 +3,15 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth(ValidRoles.user)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
