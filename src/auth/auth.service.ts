dced711d64f987e23927f01420a76b556b37d376
Dieguidev <diegogaraycullas@gmail.com>
2024-04-28 22:23:56 -0500

    implementado strategia de jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
+
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt'
+import { CreateUserDto, LoginUserDto } from './dto';
 
 
 @Injectable()
@@ -34,6 +35,19 @@ export class AuthService {
       this.handleDBExceptions(error)
 
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    })
+    if (!user) throw new UnauthorizedException('Credentials are not valid')
+
+    if(!bcrypt.compareSync(password, user.password)) throw new UnauthorizedException('Credentials are not valid')
+
+    return user
   }
 
   // findAll() {
